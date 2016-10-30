c07c71e72263b1cd68d58ca475b05743efe82f9a
“Zhangzhenguo3352git config --global user.email “13012113210@163.com <13012113210@163.comgit config --global user.email 13012113210@163.com>
2016-10-30 20:48:43 +0800

    显示在线人数和私聊

---
+++ Object.keys.js
@@ -0,0 +1,4 @@
+// Object.keys(xx)  可以直接把 key 取出来放在 数组里
+var json = {name1:123,name2:456}
+// [ 'name1', 'name2' ]
+console.log(Object.keys(json))
--- charRoom-2/server.js
+++ charRoom-2/server.js
@@ -1,27 +1,57 @@
 var express = require('express');
 var app = express();
 //这个页面 要实现（广播型 + 私聊） 聊天室
+app.use(express.static(__dirname)) // 纠正引入 文件路径 
 app.get('/',function(req,res){
 	res.sendfile('./index.html')
 })
 var server = require('http').createServer(app)
 var io = require('socket.io')(server)
+// 把 注册的 用户 暂时放在 一个 json 里
+var users = {};
 
 io.on('connection',function(socket){
-	console.log('ok')
-	socket.emit('message',{info:'欢迎来到聊天室，请输入匿名 ^ . ^ ! '})
+	
+	// console.log('ok')
+	socket.emit('message',{user:'',info:'欢迎来到聊天室，请输入匿名 ^ . ^ ! '})
 	var username;
+
+	// 接收 客户端发送的 事件
+	socket.on('us_info',function(){
+		// 给所有人发送 用户有几个，把他们的名字发送出去
+		io.emit('us_info',Object.keys(users))
+	})
+
 	socket.on('message',function(msg){
+		// console.log(Object.keys(users))  // [ 'name1', 'name2' ]
+		
 		// 得到前台的 input的value值 ，通知给所有人 io
+		
 		if(username){
-			// 如果用户写了匿名
-			// 广播型发送消息
-			io.emit('message',{info:'(' +username+ ') 说：'+msg})
+			
+			var reg = msg.match(/@(.+)\s(.+)/) // [ '@sda ', 'sda', index: 0, input: '@sda ' ]
+			if(reg){
+				var objUser = reg[1] // 用户名
+				var mas_content = reg[2] // 发给某个用户的话
+				//得到对方的 socket 然后发送消息
+				// 发送方就是本人
+				users[objUser].emit('message',{user:username,info:' 私聊给 ('+objUser+') 说：'+mas_content})
+				//让自己也看到 私聊的信息
+				socket.emit('message',{user:username,info:' 私聊给 ('+objUser+') 说：'+mas_content})
+			}else{
+				// 如果用户写了匿名
+				// 广播型发送消息
+				io.emit('message',{user:username,info:' 广播说：'+msg})
+			}
+			
 		}else{
 			
-			// 如果用户没有写匿名
-			username = msg // 把用户第一次 输入的，当成匿名
-			io.emit('message',{info:'系统：（' +username+ '）来到了房间'})
+				username = msg
+				users[username] = socket
+				//发送用户信息时，给客户端发送 就发送 客户端有几个
+				io.emit('us_info',Object.keys(users))
+				io.emit('message',{user:'', info:'系统：欢迎<span class="color_399">（' +username+ '）</span>来到了房间'})
+	
 
 		}
 	})
